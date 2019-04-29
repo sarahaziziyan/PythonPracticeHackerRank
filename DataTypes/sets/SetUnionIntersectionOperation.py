3f49e61eb0cb6194f7e2499f474878638ab57fba
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-04-29 17:51:21 +0430

    setDifference learned
    Signed-off-by: Sarah Aziziyan <aziziyan.sarah@gmail.com>

--- DataTypes/sets/SetUnionIntersectionOperation.py
+++ DataTypes/sets/SetUnionIntersectionOperation.py
@@ -5,10 +5,16 @@ frenchhSet = set(map(int, input().split()))
 
 union = englishSet.union(frenchhSet)
 intersection = englishSet.intersection(frenchhSet)
+difference = englishSet.difference(frenchhSet)
 print(englishSet)
 print(frenchhSet)
+
 print(union)
-print(intersection)
 print(union.__len__())
+
+print(intersection)
 print(intersection.__len__())
+
+print(difference)
+print(difference.__len__())
 
