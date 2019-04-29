d1714136bcb1c2e939ab15ee8ccebc93c92b5fcb
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-04-29 17:38:57 +0430

    set intersection learned
    Signed-off-by: Sarah Aziziyan <aziziyan.sarah@gmail.com>

--- DataTypes/sets/SetunionOperation.py
+++ DataTypes/sets/SetunionOperation.py
@@ -4,8 +4,11 @@ m = int(input())
 frenchhSet = set(map(int, input().split()))
 
 union = englishSet.union(frenchhSet)
+intersection = englishSet.intersection(frenchhSet)
 print(englishSet)
 print(frenchhSet)
 print(union)
+print(intersection)
 print(union.__len__())
+print(intersection.__len__())
 
