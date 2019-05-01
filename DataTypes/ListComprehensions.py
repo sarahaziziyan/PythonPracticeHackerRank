7d412dd250e81e97afc776ef0aa78e77c6724e17
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2019-05-01 17:20:58 +0430

    solve ListComprehension
    Signed-off-by: Sarah Aziziyan <aziziyan.sarah@gmail.com>

--- DataTypes/ListComprehensions.py
+++ DataTypes/ListComprehensions.py
@@ -13,3 +13,11 @@ from pip._vendor.distlib.compat import raw_input
 #             p+=1
 #
 # print(ar)
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)])
